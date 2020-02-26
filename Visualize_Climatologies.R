bb8dfe7042bf79c24119c5663b4f67e8f74ec35f
Kisei Tanaka <kisei.tanaka@gmail.com>
2020-02-26 11:54:49 -0800

    dgfg

--- Visualize_Climatologies.R
+++ Visualize_Climatologies.R
@@ -10,6 +10,7 @@ library(rworldmap)
 library(ggplot2)
 library(geosphere)
 library(gpclib)
+library(ggalt)
 
 rm(list = ls())
 
@@ -36,12 +37,113 @@ plot_clim = function(data, mode){
   Baseline <- df[[1:720]] 
   names(Baseline)
   
-  if (mode == "mean") stat = calc(Baseline, mean)
-  if (mode == "sd") stat = calc(Baseline, sd)
+  if (mode == "mean") {
+    
+    jan = calc(Baseline[[seq(1, 719, 12)]], mean)
+    jan = as.data.frame(rasterToPoints(jan))
+    jan$month = "jan"
+
+    feb = calc(Baseline[[seq(2, 719, 12)]], mean)
+    feb = as.data.frame(rasterToPoints(feb))
+    feb$month = "feb"
+    
+    mar = calc(Baseline[[seq(3, 719, 12)]], mean)
+    mar = as.data.frame(rasterToPoints(mar))
+    mar$month = "mar"
+    
+    apr = calc(Baseline[[seq(4, 719, 12)]], mean)
+    apr = as.data.frame(rasterToPoints(apr))
+    apr$month = "apr"
+    
+    may = calc(Baseline[[seq(5, 719, 12)]], mean)
+    may = as.data.frame(rasterToPoints(may))
+    may$month = "may"
+    
+    jun = calc(Baseline[[seq(6, 719, 12)]], mean)
+    jun = as.data.frame(rasterToPoints(jun))
+    jun$month = "jun"
+    
+    jul = calc(Baseline[[seq(7, 719, 12)]], mean)
+    jul = as.data.frame(rasterToPoints(jul))
+    jul$month = "jul"
+    
+    aug = calc(Baseline[[seq(8, 719, 12)]], mean)
+    aug = as.data.frame(rasterToPoints(aug))
+    aug$month = "aug"
+    
+    sep = calc(Baseline[[seq(9, 719, 12)]], mean)
+    sep = as.data.frame(rasterToPoints(sep))
+    sep$month = "sep"
+    
+    oct = calc(Baseline[[seq(10, 719, 12)]], mean)
+    oct = as.data.frame(rasterToPoints(oct))
+    oct$month = "oct"
+    
+    nov = calc(Baseline[[seq(11, 719, 12)]], mean)
+    nov = as.data.frame(rasterToPoints(nov))
+    nov$month = "nov"
+    
+    dec = calc(Baseline[[seq(12, 719, 12)]], mean)
+    dec = as.data.frame(rasterToPoints(dec))
+    dec$month = "dec"
+    
+    stat = rbind(jan, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec)
+    
+  }
   
-  stat = rasterToPoints(stat)
-  colnames(stat) = c("x", "y", mode)
-  stat = as.data.frame(stat)
+  if (mode == "sd") {
+    
+    jan = calc(Baseline[[seq(1, 719, 12)]], sd)
+    jan = as.data.frame(rasterToPoints(jan))
+    jan$month = "jan"
+    
+    feb = calc(Baseline[[seq(2, 719, 12)]], sd)
+    feb = as.data.frame(rasterToPoints(feb))
+    feb$month = "feb"
+    
+    mar = calc(Baseline[[seq(3, 719, 12)]], sd)
+    mar = as.data.frame(rasterToPoints(mar))
+    mar$month = "mar"
+    
+    apr = calc(Baseline[[seq(4, 719, 12)]], sd)
+    apr = as.data.frame(rasterToPoints(apr))
+    apr$month = "apr"
+    
+    may = calc(Baseline[[seq(5, 719, 12)]], sd)
+    may = as.data.frame(rasterToPoints(may))
+    may$month = "may"
+    
+    jun = calc(Baseline[[seq(6, 719, 12)]], sd)
+    jun = as.data.frame(rasterToPoints(jun))
+    jun$month = "jun"
+    
+    jul = calc(Baseline[[seq(7, 719, 12)]], sd)
+    jul = as.data.frame(rasterToPoints(jul))
+    jul$month = "jul"
+    
+    aug = calc(Baseline[[seq(8, 719, 12)]], sd)
+    aug = as.data.frame(rasterToPoints(aug))
+    aug$month = "aug"
+    
+    sep = calc(Baseline[[seq(9, 719, 12)]], sd)
+    sep = as.data.frame(rasterToPoints(sep))
+    sep$month = "sep"
+    
+    oct = calc(Baseline[[seq(10, 719, 12)]], sd)
+    oct = as.data.frame(rasterToPoints(oct))
+    oct$month = "oct"
+    
+    nov = calc(Baseline[[seq(11, 719, 12)]], sd)
+    nov = as.data.frame(rasterToPoints(nov))
+    nov$month = "nov"
+    
+    dec = calc(Baseline[[seq(12, 719, 12)]], sd)
+    dec = as.data.frame(rasterToPoints(dec))
+    dec$month = "dec"
+    
+    stat = rbind(jan, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec)
+    
+  }
   
   # pdf(paste0("~/Dropbox (MBA)/PAPER Kisei heat extremes/figures/Climatologies/", data, "_Climatology_1870-1929.pdf"), height = 10, width = 8.5)
   # par(mfrow = c(2,1))
@@ -69,14 +171,14 @@ sd = rbind(d4, d5, d6)
 world <- ne_countries(scale = "small", returnclass = "sf") 
 
 p = mean %>% 
-  sample_frac(1) %>%
+  sample_frac(0.001) %>%
   ggplot() + 
-  geom_point(aes(x, y, color = mean)) + 
+  geom_point(aes(x, y, color = layer)) + 
   geom_polygon(data = world.df, aes(x = long, y = lat, group = group)) +
   scale_color_gradientn(colors = matlab.like(100), "", limits = c(-2, 29.4)) +
   # scale_x_continuous(expand = c(-10, 0), "") +
   # scale_y_continuous(expand = c(0, 0), "") +
-  facet_wrap(.~source) + 
+  facet_grid(source ~ month) + 
   # coord_sf(xlim = range(d$x), ylim = range(d$y)) +
   coord_map("ortho", orientation = c(10, 250, 0)) +
   # coord_map("moll") +
@@ -94,16 +196,26 @@ print(p)
 
 world <- fortify(getMap())
 
+mean$month = factor(mean$month, levels=c('jan', 'feb', 'mar', 
+                                         'apr', 'may', 'jun', 
+                                         'jul', 'aug', 'sep', 
+                                         'oct', 'nov', 'dec'))
+
+sd$month = factor(sd$month, levels=c('jan', 'feb', 'mar', 
+                                     'apr', 'may', 'jun', 
+                                     'jul', 'aug', 'sep', 
+                                     'oct', 'nov', 'dec'))
+
 p1 = mean %>% 
   sample_frac(1) %>%
   ggplot() + 
-  geom_point(aes(x = x, y = y, color = mean), size = 0.5, alpha = 0.5) +
+  geom_point(aes(x = x, y = y, color = layer), size = 0.5, alpha = 0.5) +
   geom_map(data = world, map = world, aes(x = long, y = lat, map_id = id),
-           color = "black", fill = "gray", size = 0.1) + 
-  scale_color_gradientn(colors = matlab.like(100), "Mean", limits = c(-2, 29.4)) +
+           color = "black", fill = "gray", size = 0.1) +
+  scale_color_gradientn(colors = matlab.like(100), "mean", limits = c(-2, 29.4)) +
   coord_proj("+proj=wintri") +
-  facet_wrap(.~ source, ncol = 3) + 
-  theme_pubr()+
+  facet_grid(source ~ month) + 
+  theme_pubr(I(9)) +
   theme(axis.title.x = element_blank(),
         axis.title.y = element_blank(), 
         axis.text.x = element_blank(),
@@ -116,13 +228,13 @@ p1 = mean %>%
 p2 = sd %>% 
   sample_frac(1) %>%
   ggplot() + 
-  geom_point(aes(x = x, y = y, color = sd), size = 0.5, alpha = 0.5) +
+  geom_point(aes(x = x, y = y, color = layer), size = 0.5, alpha = 0.5) +
   geom_map(data = world, map = world, aes(x = long, y = lat, map_id = id),
-           color = "black", fill = "gray", size = 0.1) + 
-  scale_color_gradientn(colors = matlab.like(100), "SD", limits = c(0, 9.1)) +
+           color = "black", fill = "gray", size = 0.1) +
+  scale_color_gradientn(colors = matlab.like(100), "sd", limits = c(0, 3.7)) +
   coord_proj("+proj=wintri") +
-  facet_wrap(.~ source, ncol = 3) + 
-  theme_pubr()+
+  facet_grid(source ~ month) + 
+  theme_pubr(I(9)) +
   theme(axis.title.x = element_blank(),
         axis.title.y = element_blank(), 
         axis.text.x = element_blank(),
@@ -132,6 +244,8 @@ p2 = sd %>%
         legend.position = "bottom", 
         legend.justification = c(1,0))
 
-pdf("~/Desktop/Climatologies_1870-1929.pdf", width = 10, height = 7)
+
+compactPDF("~/Desktop/Climatologies_1870-1929.pdf", gs_quality='screen')
+# png("~/Desktop/Climatologies_1870-1929.png", res = 300, units = "in", width = 15, height = 7)
 cowplot::plot_grid(p1, p2, nrow = 2)
 dev.off()
