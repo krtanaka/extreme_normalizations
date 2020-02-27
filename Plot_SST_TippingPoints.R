546c237a76f9db750f776ef2345b543b3d01dc59
Kisei Tanaka <kisei.tanaka@gmail.com>
2020-02-27 13:39:40 -0800

    hkgf

--- Plot_SST_TippingPoints.R
+++ Plot_SST_TippingPoints.R
@@ -6,7 +6,6 @@ hadi$source = "HadISSTv1.1"
 cobe$source = "COBEv2"
 er$source = "ERSSTv4"
 
-
 library(colorRamps)
 pdf("~/Desktop/Tipping_Posint.pdf", height = 5, width = 6)
 plot(cobe$year_sum, type = "o", axes = F, pch = 20, xlab = "", ylab = "", col = matlab.like(10)[8])
@@ -15,15 +14,45 @@ points(er$year_sum, type = "o", pch = 20, xlab = "", ylab = "", ylab = "", col =
 axis(1)
 axis(2, las = 2, at = seq(0, 0.8, 0.1))
 abline(h = 0.5, lty = 2)
-legend("topleft", c("COBEv2", "HadISSTv1.1", "ERSSTv4"), lty = 1, col = c(matlab.like(10)[8], 
-                                                                          matlab.like(10)[9], 
-                                                                          matlab.like(10)[10]), 
+legend("topleft", c("COBEv2", "HadISSTv1.1", "ERSSTv4"), 
+       lty = 1, col = c(matlab.like(10)[8], 
+                        matlab.like(10)[9], 
+                        matlab.like(10)[10]), lwd = 2,
        bty = "n")
 dev.off()
 
-df = rbind(hadi, cobe, er)
 
+df = rbind(hadi, cobe, er)
+rownames(df) <- c()
+subset()
+
+df = tidyr::separate(df, time, c("Year", "Month"), sep = "-")
+
+df$Month <- sprintf("%02d", as.numeric(df$Month))
+df$Day = 01
+df$Day <- sprintf("%02d", as.numeric(df$Day))
+df$Time = paste(df$Year, df$Month, df$Day, sep = "-")
+df$Time = as.Date(df$Time)
+
+cobe_date = paste(subset(df, source == "COBEv2" & year_sum > 0.5)[1,2:3], collapse = "")
+hadi_date = paste(subset(df, source == "HadISSTv1.1" & year_sum > 0.5)[1,2:3], collapse = "")
+er_date = paste(subset(df, source == "ERSSTv4" & year_sum > 0.5)[1,2:3], collapse = "")
 
 library(ggplot2)
-ggplot(df, aes(year_sum, time, color = source)) + 
-         geom_line()
+library(ggdark)
+library(ggpubr)
+
+ggplot(data = df, aes(x = Time, y = year_sum, color = source, group = source)) +
+  # geom_point(alpha = 0.8) +
+  geom_line(alpha = 1) +
+  geom_hline(yintercept = 0.5, linetype = "dashed", color = "gray") + 
+  labs(x = "", y = "") +
+  scale_color_manual(values = topo.colors(3), "") +
+  scale_x_date(breaks = seq(as.Date("1900-01-01"), as.Date("2018-12-01"), by="10 years"), 
+               labels = scales::date_format("%Y")) +
+  dark_theme_classic(I(20)) +
+  # theme_pubr() +
+  # facet_wrap(.~Month) + 
+  theme(legend.position = c(0.14, 0.95), 
+        axis.text.x = element_text(angle = 90, hjust = 1))
+
