2a30c19022ac19b617119a7d1d9929bb9550e55b
Kisei Tanaka <kisei.tanaka@gmail.com>
2020-02-28 09:52:44 -0800

    ssgd

--- Plot_SST_TippingPoints.R
+++ Plot_SST_TippingPoints.R
@@ -8,6 +8,7 @@ er$source = "ERSSTv4"
 
 library(colorRamps)
 pdf("~/Desktop/Tipping_Posint.pdf", height = 5, width = 6)
+par(mar = c(2,3,1,2))
 plot(cobe$year_sum, type = "o", axes = F, pch = 20, xlab = "", ylab = "", col = matlab.like(10)[8])
 points(hadi$year_sum, type = "o", pch = 20, xlab = "", ylab = "", ylab = "", col = matlab.like(10)[9])
 points(er$year_sum, type = "o", pch = 20, xlab = "", ylab = "", ylab = "", col = matlab.like(10)[10])
@@ -21,10 +22,8 @@ legend("topleft", c("COBEv2", "HadISSTv1.1", "ERSSTv4"),
        bty = "n")
 dev.off()
 
-
 df = rbind(hadi, cobe, er)
 rownames(df) <- c()
-subset()
 
 df = tidyr::separate(df, time, c("Year", "Month"), sep = "-")
 
@@ -42,17 +41,37 @@ library(ggplot2)
 library(ggdark)
 library(ggpubr)
 
-ggplot(data = df, aes(x = Time, y = year_sum, color = source, group = source)) +
+pdf("~/Desktop/Tipping_Points_Year_Month.pdf", height = 8, width = 16)
+p1 = ggplot(data = df, aes(x = Time, y = year_sum, color = source, group = source)) +
   # geom_point(alpha = 0.8) +
-  geom_line(alpha = 1) +
+  geom_line() +
   geom_hline(yintercept = 0.5, linetype = "dashed", color = "gray") + 
   labs(x = "", y = "") +
-  scale_color_manual(values = topo.colors(3), "") +
+  scale_color_manual(values = heat.colors(3), "") +
+  # scale_color_viridis_d() + 
   scale_x_date(breaks = seq(as.Date("1900-01-01"), as.Date("2018-12-01"), by="10 years"), 
+               labels = scales::date_format("%Y")) +
+  # dark_theme_classic(I(20)) +
+  theme_pubr() +
+  # facet_wrap(.~Month) +
+  theme(legend.position = c(0.15, 0.95), 
+        axis.text.x = element_text(angle = 90, hjust = 1))
+
+p2 = ggplot(data = df, aes(x = Time, y = year_sum, color = source, group = source)) +
+  # geom_point(alpha = 0.8) +
+  geom_line() +
+  geom_hline(yintercept = 0.5, linetype = "dashed", color = "gray") + 
+  labs(x = "", y = "") +
+  scale_color_manual(values = heat.colors(3), "") +
+  scale_x_date(breaks = seq(as.Date("1900-01-01"), as.Date("2018-12-01"), by = "25 years"), 
                labels = scales::date_format("%Y")) +
   dark_theme_classic(I(20)) +
   # theme_pubr() +
-  # facet_wrap(.~Month) + 
-  theme(legend.position = c(0.14, 0.95), 
+  facet_wrap(.~Month) +
+  theme(legend.position = "none", 
         axis.text.x = element_text(angle = 90, hjust = 1))
+
+cowplot::plot_grid(p1, p2)
+
+dev.off()
 
