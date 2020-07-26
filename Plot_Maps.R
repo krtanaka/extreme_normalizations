25861de8d31d904456fe5e7eb7ac0d2d1b61ed75
Kisei Tanaka <kisei.tanaka@gmail.com>
2020-07-26 00:04:12 -0700

    ok

--- Plot_Maps.R
+++ Plot_Maps.R
@@ -69,17 +69,15 @@ map = function(mode){
   load(paste0("/Users/ktanaka/extreme_normalizations/results/COBE/SST_Anomalies_1990-1999_", cutoff, ".RData")); cobe2 = anom; cobe2$source = "COBE v2"; cobe2$period = "1990-1999"
   load(paste0("/Users/ktanaka/extreme_normalizations/results/COBE/SST_Anomalies_2000-2009_", cutoff, ".RData")); cobe3 = anom; cobe3$source = "COBE v2"; cobe3$period = "2000-2009"
   load(paste0("/Users/ktanaka/extreme_normalizations/results/COBE/SST_Anomalies_2010-2019_", cutoff, ".RData")); cobe4 = anom; cobe4$source = "COBE v2"; cobe4$period = "2010-2019"
-  load(paste0("/Users/ktanaka/extreme_normalizations/results/ER/SST_Anomalies_1980-1989_", cutoff, ".RData")); er1 = anom; er1$source = "ERSST v4"; er1$period = "1980-1989"
-  load(paste0("/Users/ktanaka/extreme_normalizations/results/ER/SST_Anomalies_1990-1999_", cutoff, ".RData")); er2 = anom; er2$source = "ERSST v4"; er2$period = "1990-1999"
-  load(paste0("/Users/ktanaka/extreme_normalizations/results/ER/SST_Anomalies_2000-2009_", cutoff, ".RData")); er3 = anom; er3$source = "ERSST v4"; er3$period = "2000-2009"
-  load(paste0("/Users/ktanaka/extreme_normalizations/results/ER/SST_Anomalies_2010-2018_", cutoff, ".RData")); er4 = anom; er4$source = "ERSST v4"; er4$period = "2010-2019"
+  load(paste0("/Users/ktanaka/extreme_normalizations/results/ER/SST_Anomalies_1980-1989_", cutoff, ".RData")); er1 = anom; er1$source = "ERSST v5"; er1$period = "1980-1989"
+  load(paste0("/Users/ktanaka/extreme_normalizations/results/ER/SST_Anomalies_1990-1999_", cutoff, ".RData")); er2 = anom; er2$source = "ERSST v5"; er2$period = "1990-1999"
+  load(paste0("/Users/ktanaka/extreme_normalizations/results/ER/SST_Anomalies_2000-2009_", cutoff, ".RData")); er3 = anom; er3$source = "ERSST v5"; er3$period = "2000-2009"
+  load(paste0("/Users/ktanaka/extreme_normalizations/results/ER/SST_Anomalies_2010-2019_", cutoff, ".RData")); er4 = anom; er4$source = "ERSST v5"; er4$period = "2010-2019"
   
   #all periods
   anom = rbind(hadi1, hadi2, hadi3, hadi4, 
                cobe1, cobe2, cobe3, cobe4,
                er1, er2, er3, er4)
-  
-  anom[anom=="ERSST v4"]<-"ERSST v5"
   
   anom$source = factor(anom$source, levels = c("HadISST v1.1", "COBE v2",  "ERSST v5"))
   
