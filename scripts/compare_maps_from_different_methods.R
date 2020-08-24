5697f93992b13d42a3b4d41372eec6b51ba9e79b
Kisei Tanaka <kisei.tanaka@gmail.com>
2020-08-24 12:13:36 -0700

    FIXED Fig 4.

--- scripts/compare_maps_from_different_methods.R
+++ scripts/compare_maps_from_different_methods.R
@@ -8,19 +8,20 @@ library(ggplot2)
 world <- fortify(rworldmap::getMap())
 range01 <- function(x){(x-min(x))/(max(x)-min(x))}
 
-#IPCC style anomalies
+#IPCC style SST anomalies
 load("~/extreme_normalizations/results/COBE/anomalies_2019_ipcc.RData"); cobe_ipcc = anom; cobe_ipcc$data = "COBE"; cobe_ipcc$sum = rowMeans(cobe_ipcc[3:14])
 load("~/extreme_normalizations/results/HadI/anomalies_2019_ipcc.RData"); hadi_ipcc = anom; hadi_ipcc$data = "HadI"; hadi_ipcc$sum = rowMeans(hadi_ipcc[3:14])
 anom_ipcc = rbind(cobe_ipcc, hadi_ipcc) %>% dplyr::select(x, y, sum) %>% group_by(x, y) %>% summarise(anom = mean(sum))
-anom_ipcc$anom = range01(anom_ipcc$anom)
+quantile(anom_ipcc$anom, 0.999)
 
-#Extreme index for 2019
+#Extreme Index for 2019
 load("~/extreme_normalizations/results/HadI/anomalies_2019_0.95.rdata"); hadi_extreme = anom; hadi_extreme$data = "HadI"
 load("~/extreme_normalizations/results/COBE/anomalies_2019_0.95.rdata"); cobe_extreme = anom; cobe_extreme$data = "COBE"
 anom_extreme = rbind(cobe_extreme, hadi_extreme) %>% dplyr::select(x, y, sum) %>% group_by(x, y) %>% summarise(anom = mean(sum))
 anom_extreme$anom = range01(anom_extreme$anom)
+quantile(anom_extreme$anom, 0.999)
 
-#IPCC - Temperature -
+#IPCC colors
 ipcc_temp <- c(rgb(103, 0, 31, maxColorValue = 255, alpha = 255),
                rgb(178, 24, 43, maxColorValue = 255, alpha = 255),
                rgb(214, 96, 77, maxColorValue = 255, alpha = 255),
@@ -33,27 +34,27 @@ ipcc_temp <- c(rgb(103, 0, 31, maxColorValue = 255, alpha = 255),
                rgb(33, 102, 172, maxColorValue = 255, alpha = 255),
                rgb(5, 48, 97, maxColorValue = 255, alpha = 255))
 
-ipcc_precip <- c(rgb(84, 48, 5, maxColorValue = 255, alpha = 255),
-                 rgb(140, 81, 10, maxColorValue = 255, alpha = 255),
-                 rgb(191, 129, 45, maxColorValue = 255, alpha = 255),
-                 rgb(223, 194, 125, maxColorValue = 255, alpha = 255),
-                 rgb(246, 232, 195, maxColorValue = 255, alpha = 255),
-                 rgb(245, 245, 245, maxColorValue = 255, alpha = 255),
-                 rgb(199, 234, 229, maxColorValue = 255, alpha = 255),
-                 rgb(128, 205, 193, maxColorValue = 255, alpha = 255),
-                 rgb(53, 151, 143, maxColorValue = 255, alpha = 255),
-                 rgb(1, 102, 94, maxColorValue = 255, alpha = 255),
-                 rgb(0, 60, 48, maxColorValue = 255, alpha = 255))
+# ipcc_precip <- c(rgb(84, 48, 5, maxColorValue = 255, alpha = 255),
+#                  rgb(140, 81, 10, maxColorValue = 255, alpha = 255),
+#                  rgb(191, 129, 45, maxColorValue = 255, alpha = 255),
+#                  rgb(223, 194, 125, maxColorValue = 255, alpha = 255),
+#                  rgb(246, 232, 195, maxColorValue = 255, alpha = 255),
+#                  rgb(245, 245, 245, maxColorValue = 255, alpha = 255),
+#                  rgb(199, 234, 229, maxColorValue = 255, alpha = 255),
+#                  rgb(128, 205, 193, maxColorValue = 255, alpha = 255),
+#                  rgb(53, 151, 143, maxColorValue = 255, alpha = 255),
+#                  rgb(1, 102, 94, maxColorValue = 255, alpha = 255),
+#                  rgb(0, 60, 48, maxColorValue = 255, alpha = 255))
 
 p1 = anom_ipcc %>% ggplot(aes(x, y, fill = anom)) +  
   geom_tile() + 
+  geom_point(data = subset(anom_ipcc, anom >= 1.773027), aes(x, y), color = 5, shape = 4, alpha = 0.5) + 
   geom_map(data = world, map = world, aes(x = long, y = lat, map_id = id),
            color = "gray20", fill = "gray20", size = 0.001) +
-  scale_fill_gradientn(colors = rev(ipcc_precip), "") +
+  scale_fill_gradientn(colors = rev(ipcc_temp), "") +
   scale_x_continuous(expand = c(-0.005, 0), "") +
   scale_y_continuous(expand = c(-0.005, 0), "") +
   coord_fixed() + 
-  ggtitle("SST Anomalies 2019") +
   theme(axis.title.x = element_blank(),
         axis.title.y = element_blank(),
         axis.text.x = element_blank(),
@@ -61,17 +62,18 @@ p1 = anom_ipcc %>% ggplot(aes(x, y, fill = anom)) +
         axis.ticks.x = element_blank(),
         axis.ticks.y = element_blank(),
         legend.position = "right",
-        legend.justification = c(1,0))
+        legend.justification = c(1,0)) + 
+  labs(tag = "a")
 
 p2 = anom_extreme %>% ggplot(aes(x, y, fill = anom)) +  
-  geom_tile(interpolate = T) + 
+  geom_tile() + 
+  geom_point(data = subset(anom_extreme, anom >= 1), aes(x, y), color = 5, shape = 4, alpha = 0.5) + 
   geom_map(data = world, map = world, aes(x = long, y = lat, map_id = id),
            color = "gray20", fill = "gray20", size = 0.001) +
-  scale_fill_gradientn(colors = rev(ipcc_precip), "") +
+  scale_fill_gradientn(colors = rev(ipcc_temp), "") +
   scale_x_continuous(expand = c(-0.005, 0), "") +
   scale_y_continuous(expand = c(-0.005, 0), "") +
   coord_fixed() + 
-  ggtitle("Extreme Index 2019") +
   theme(axis.title.x = element_blank(),
         axis.title.y = element_blank(),
         axis.text.x = element_blank(),
@@ -79,35 +81,11 @@ p2 = anom_extreme %>% ggplot(aes(x, y, fill = anom)) +
         axis.ticks.x = element_blank(),
         axis.ticks.y = element_blank(),
         legend.position = "right",
-        legend.justification = c(1,0))
+        legend.justification = c(1,0)) + 
+  labs(tag = "b")
 
-colnames(anom_extreme)[3] = "extreme"
-colnames(anom_ipcc)[3] = "ipcc"
+pdf(paste0("~/Desktop/Fig4_", Sys.Date(), ".pdf"), height = 6, width = 6)
 
-df = merge(anom_extreme, anom_ipcc, by = c("x", "y"))
-df$ours_theirs = df$extreme - df$ipcc
-
-p3 = df %>% ggplot(aes(x, y, fill = ours_theirs)) +  
-  geom_tile(interpolate = T) + 
-  geom_map(data = world, map = world, aes(x = long, y = lat, map_id = id),
-           color = "gray20", fill = "gray20", size = 0.001) +
-  scale_fill_gradientn(colors = rev(ipcc_precip), "") +
-  scale_x_continuous(expand = c(-0.005, 0), "") +
-  scale_y_continuous(expand = c(-0.005, 0), "") +
-  coord_fixed() + 
-  ggtitle("Ours - Theirs") +
-  theme(axis.title.x = element_blank(),
-        axis.title.y = element_blank(),
-        axis.text.x = element_blank(),
-        axis.text.y = element_blank(),
-        axis.ticks.x = element_blank(),
-        axis.ticks.y = element_blank(),
-        legend.position = "right",
-        legend.justification = c(1,0))
-
-
-pdf(paste0("~/Desktop/Fig4_", Sys.Date(), ".pdf"), height = 10, width = 10)
-
-cowplot::plot_grid(p1, p2, p3, ncol = 1)
+p1/p2
 
 dev.off()
