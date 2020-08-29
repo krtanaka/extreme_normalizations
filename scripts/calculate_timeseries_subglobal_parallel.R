346d6410bdf38516216fcadf61786bc4e9a09013
Kisei Tanaka <ktanaka@mbayaq.org>
2020-08-29 14:58:48 -0700

    ok

--- scripts/calculate_timeseries_subglobal_parallel.R
+++ scripts/calculate_timeseries_subglobal_parallel.R
@@ -19,7 +19,7 @@ p = c(0.95, 0.98)[2]
 
 calculate_anomalies = function(data){
 
-  data = c("HadI", "COBE", "ER")[3]
+  data = c("HadI", "COBE", "ER")[2]
   
   setwd(paste0("/Users/", Sys.info()[7], "/Dropbox (MBA)/PAPER Kisei heat extremes/data/"))
   
@@ -45,8 +45,6 @@ calculate_anomalies = function(data){
   #remove polar regions
   names(Target)
   latlon = Target[,c(1:2)]; plot(latlon, pch = ".")
-  library(sp)
-  library(maptools)
   coordinates(latlon) = ~x+y
   statarea <- rgdal::readOGR("/Users/ktanaka/Dropbox (MBA)/PAPER Kisei heat extremes/data/World_Seas_IHO_v1/World_Seas.shp")
   CRS.new <- CRS("+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0+datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0") #EPSG:102003
