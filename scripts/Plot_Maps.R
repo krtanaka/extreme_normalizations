d9145a3793219154750cafd08845bb5738e03195
Kisei Tanaka <1595661320@mil>
2020-12-13 14:06:18 -1000

    ok

--- scripts/Plot_Maps.R
+++ scripts/Plot_Maps.R
@@ -72,16 +72,17 @@ map = function(mode){
       subset(source %in% c("COBE v2", "HadISST v1.1")) %>%
       group_by(x, y, period) %>% 
       summarise(sum = mean(sum)) %>% 
-      # subset(y %in% seq(-70, 70, by = 0.1)) %>%
+      subset(y %in% seq(-70, 70, by = 0.1)) %>%
       ggplot() + 
       geom_point(aes(x, y, color = sum, fill = sum), size = 0.1) +
       # geom_polygon(data = world.df, aes(x = long, y = lat, group = group)) +
+      geom_map(data = world, map = world, aes(x = long, y = lat, map_id = id), color = "gray20", fill = "gray20", size = 0.001) +
       scale_color_gradientn(colors = rev(ipcc_col), "", limits = c(0, 1), breaks = c(0, 0.5, 1)) +
       scale_fill_gradientn(colors = rev(ipcc_col), "", limits = c(0, 1), breaks = c(0, 0.5, 1)) +
       scale_x_continuous(expand = c(-0.005, 0), "") +
       scale_y_continuous(expand = c(-0.005, 0), "") +
       # coord_sf(xlim = range(anom$x), ylim = range(anom$y)) +
-      # facet_grid(source ~ period) +
+      facet_grid(~period) +
       theme_minimal() +
       coord_map("ortho", orientation = c(0, 0, 0)) + #normal
       # coord_map("ortho", orientation = c(-90, 45, 0)) + #arctic centered, 0, 0, 0 gives you a normal look
@@ -102,7 +103,7 @@ map = function(mode){
       geom_point(alpha = 0.5, shape = 16) +
       geom_map(data = world, map = world, aes(x = long, y = lat, map_id = id), color = "gray20", fill = "gray20", size = 0.001) + 
       scale_color_gradientn(colors = rev(ipcc_col), "", limits = c(0,1), breaks = c(0,0.5,1)) +
-      coord_proj("+proj=wintri") +
+      # coord_proj("+proj=wintri") +
       # facet_grid(source ~ period) +
       # facet_grid(~ period) +
       theme_minimal(I(20)) +
